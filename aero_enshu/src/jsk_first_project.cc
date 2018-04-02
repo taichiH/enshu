75c4ccec7040a408aaf5c3840143d3ae6bb9ded4
higashide <higashide@jsk.imi.i.u-tokyo.ac.jp>
2018-04-02 22:53:39 +0900

    modified jsk_first_project.cc

--- aero_enshu/src/jsk_first_project.cc
+++ aero_enshu/src/jsk_first_project.cc
@@ -2,9 +2,7 @@
   In this demo, a robot will pick up a caffelatte
   or enter an error recovery mode when nothing is found.
 */
-
 #define MAX 5
-
 #include "negomo/NegomoLib2.hh"
 #include "aero_devel_lib/devel_lib.hh"
 
@@ -14,6 +12,7 @@ negomo_lib::NegomoBridge2Ptr planner_; // planner
 Eigen::Vector3d pos_;
 std::vector<Eigen::Vector3d> results_buf_;
 std::map<std::string, std::function<void(int)> > pre_;
+int index_;
 
 // when entering error
 int error(int _inhands, int &_nexttask) {
@@ -23,7 +22,6 @@ int error(int _inhands, int &_nexttask) {
 };
 
 int containerPose(int _inhands, int &_nexttask){
-  // robot_->goPos(0.0, 0.0, 0.0);
   robot_->moveTo("s_shelf_container");
   while(robot_->isMoving() && ros::ok()){
     usleep(200*1000);
@@ -33,16 +31,15 @@ int containerPose(int _inhands, int &_nexttask){
 }
 
 int shelfPose(int _inhands, int &_nexttask){
-  lib_->lookShelf();
+  lib_->lookShelfFront();
   robot_->moveTo("s_shelf");
   while(robot_->isMoving() && ros::ok()){
     usleep(200*1000);
   }
-
   return _inhands;
 }
 
-void visualize(){
+void visualizeMarker(){
   aero::Transform obj_pos;
   for(int i=0; i<results_buf_.size(); ++i){
     obj_pos = aero::Translation(results_buf_.at(i));
@@ -50,47 +47,58 @@ void visualize(){
   }
 }
 
+int watchInteraction(int _inhands, int &_nexttask) {
+  if(index_ > 1)
+    results_buf_.clear();
+  return _inhands;
+}
+
 int watch(int _inhands, int &_nexttask) {
   lib_->setFCNModel("final"); // set recognition model
-  std::vector<aero_recognition_msgs::Scored2DBox> hand_result = lib_->recognizeHand();
+  std::vector<aero_recognition_msgs::Scored2DBox> hand_recognition_result
+    = lib_->recognizeHand();
   std::vector<Eigen::Vector3d> results;
   bool found = lib_->findItem("pie", results);
   ROS_INFO("finished");
 
-  if(!hand_result.empty()){
+  if(!hand_recognition_result.empty()){
     ROS_INFO("hand detect!");
     return _inhands;
   }
+
   if(!found){
-    ROS_INFO("not found");
-    return _inhands;
-  }
-  if(results_buf_.empty()){
-    results_buf_.push_back(results.at(0));
+    ROS_INFO("object not found");
     return _inhands;
   }
 
   const double diff_min = 0.08;
-  for(int i=0; i<results.size(); ++i){
-    Eigen::Vector3d diff = results.at(i) - results_buf_.at(0);
-    if(diff.norm() > diff_min){
-      results_buf_.push_back(results.at(i));
-      for(int j=2; j<MAX; ++j){
-        results_buf_.push_back(results_buf_.at(j-1) + diff);
+
+  if(index_ > 1){
+    ROS_INFO("index > 1");
+  } else {
+    if(results_buf_.empty()){
+      results_buf_.push_back(results.at(0));
+      return _inhands;
+    }
+    for(int i=0; i<results.size(); ++i){
+      Eigen::Vector3d diff = results.at(i) - results_buf_.at(0);
+      if(diff.norm() > diff_min){
+        results_buf_.push_back(results.at(i));
+        for(int j=2; j<MAX; ++j){
+          results_buf_.push_back(results_buf_.at(j-1) + diff);
+        }
+        planner_->getEntities().put("loopCondition", false);
+        break;
       }
-      planner_->getEntities().put("loopCondition", false);
-      break;
     }
   }
-
-  visualize();
-
+  visualizeMarker();
   return _inhands;
 };
 
 int put(int _inhands, int &_nexttask) {
-  int index = planner_->getEntities().get<int>("index", 2);
-  Eigen::Vector3d shelf_initial(results_buf_.at(index).x(), results_buf_.at(index).y(), results_buf_.at(index).z());
+  index_ = planner_->getEntities().get<int>("index", 2);
+  Eigen::Vector3d shelf_initial(results_buf_.at(index_).x(), results_buf_.at(index_).y(), results_buf_.at(index_).z());
 
   aero::Transform obj_pos;
   for(int i=0; i<results_buf_.size(); ++i){
@@ -104,7 +112,8 @@ int put(int _inhands, int &_nexttask) {
 
   // start tracking object
   robot_->setTrackingMode(true);
-  robot_->setLookAt(shelf_initial, false, true); // look at target
+  // look at target
+  robot_->setLookAt(shelf_initial, false, true);
   // start interaction on background
   planner_->iStart(negomo_lib::jumpSettings(), ws);
   ROS_INFO("place coffee reach");
@@ -123,26 +132,22 @@ int put(int _inhands, int &_nexttask) {
   ROS_INFO("place coffee return");
   lib_->placeCoffeeReturn();
   lib_->sendResetPose();
-  
+
   // finich tracking object
   robot_->setTrackingMode(false);
-  std::cerr << "indes: " << index << std::endl;
-  if(index == MAX-1){
+  if(index_ == MAX-1){
     ROS_INFO("put loop end");
     planner_->getEntities().put("loopCondition", false);
     return lib_->getUsingHandsNum();
   }
-  planner_->getEntities().put("index", index+1);
-
+  planner_->getEntities().put("index", index_+1);
   return lib_->getUsingHandsNum();
 };
 
-// picking action
 int pick(int _inhands, int &_nexttask) {
-  lib_->setFCNModel("final"); // set recognition model
-
+  lib_->setFCNModel("final");
   // get position of item
-  bool found = lib_->poseAndRecognize("container", "pie", pos_, -0.12);
+  bool found = lib_->poseAndRecognize("container", "pie", pos_, -0.2);
 
   if (found)
     found = lib_->pickCoffeeFront(pos_, 0.83);
@@ -152,7 +157,6 @@ int pick(int _inhands, int &_nexttask) {
   }
 
   lib_->sendResetPose();
-  
   return lib_->getUsingHandsNum();
 };
 
@@ -194,7 +198,6 @@ int main(int argc, char **argv) {
   // add preinteraction callback to planner
   planner_->initlib(boost::bind(&preinteractionCb, _1, _2));
 
-
   // create actions for planner
   negomo_lib::ActionList exceptions =
     {std::make_tuple(0, 0, error, planner_->emptyAction, "error"),
@@ -203,6 +206,7 @@ int main(int argc, char **argv) {
   negomo_lib::ActionList task0 =
     {std::make_tuple(0, 0, planner_->initTask, planner_->emptyAction, "init"),
      std::make_tuple(0, 1, shelfPose, planner_->emptyAction, "shelf"),
+     std::make_tuple(0, 0, watchInteraction, planner_->emptyAction, "interaction"),
      std::make_tuple(0, 0, planner_->loopStart, planner_->emptyAction, "loop"),
      std::make_tuple(0, 0, watch, planner_->emptyAction, "watch"),
      std::make_tuple(0, 0, planner_->loopEnd, planner_->emptyAction, "loop"),
